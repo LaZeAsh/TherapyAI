203f114307e7208891de7da8e84f02ee08a5b295
LaZeAsh <ayushrgarg@gmail.com>
2022-11-26 01:18:12 -0500

    make voice recognition work and computer speaking work

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TherapyAI
+
+### Getting Started
+
+Macbook specific instructions
+```bash
+pip3 install virtualenv # install virtualenv if you haven't already
+
+virtualenv env # Start a virtual environment
+source env/bin/activate # Activate a virtual environment
+pip install -r requirements.txt
+```
---
+++ voice_recognition.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+import os
+r = sr.Recognizer()
+# use recognize_google() for google web speech api (no install req)
+# limited to 50 requests per day so we might need to use recognize_sphinx
+
+mic = sr.Microphone(device_index=2) # index 2 because for me thats macbook mic (might differ for you)
+
+# try catch statement to handle audio that cannot be transcribed
+try:
+    with mic as source:
+        print("executing")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+        speech = r.recognize_google(audio)
+        print(speech)
+        # testing to make it so that the system speaks something (won't be in this format)
+        # look for alternatives to this it sounds too robot(y)
+        os.system(f"say '{speech}'")
+except:
+    pass
+        
